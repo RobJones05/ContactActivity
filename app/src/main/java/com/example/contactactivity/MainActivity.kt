4d2e51455ce2489d2b11b0330e08cce81d645228
RobJones05 <roberttamlinjones@gmail.com>
2026-03-03 11:19:23 +0200

    Did some more stuff

--- app/src/main/java/com/example/contactactivity/MainActivity.kt
+++ app/src/main/java/com/example/contactactivity/MainActivity.kt
@@ -12,14 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val firstName: String = "John"
-        val lastName: String = "Doe"
+        val firstName: String = "Bobby"
+        val lastName: String = "Rizzler"
         val address: String = "155 Maple Rd"
         val isMale: Boolean = true
         val dayOfBirth: Int = 9
         val monthOfBirth: Int = 5
         val yearOfBirth: Int = 2000
-        
+
+        Log.d("MainActivity",
+            " Hello, your name is bomboclaat Name: $firstName $lastName, Address: $address, Male: $isMale, DOB: $dayOfBirth/$monthOfBirth/$yearOfBirth"
         )
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
